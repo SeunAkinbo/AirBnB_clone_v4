d1acb710fd0bc4d05704d92803cd5fbe3b259587
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-05-05 21:02:27 +0000

    Updating cache_id && route

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -5,6 +5,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from uuid import uuid4
 from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -37,7 +38,7 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places, cache_id=uuid4())
 
 
 if __name__ == "__main__":
