de51c0bd87298a020e17438ba36f0f865fd161b6
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-05-05 22:46:10 +0100

    Updating Amenity

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,23 @@
 /* The script executs only when DOM is loaded
- * JQuery is mandatory
- * Listen for changes on each input checkbox tag:
- * 	if the checkbox is checked, you must store the Amenity ID in a
- * 	variable (dictionary or list)
- * 	if the checkbox is unchecked, you must remove the Amenity ID
- * 	from the variable
- * 	update the h4 tag inside the div Amenities with the list of
- * 	Amenities checked
+   JQuery is mandatory
+   Listen for changes on each input checkbox tag:
+  	 if the checkbox is checked, you must store the Amenity ID in a
+  	 variable (dictionary or list)
+  	 if the checkbox is unchecked, you must remove the Amenity ID
+  	 from the variable
+  	 update the h4 tag inside the div Amenities with the list of
+  	 Amenities checked
  */
+
+$(document).ready(function () {
+  const amenity = {};
+  $(".amenities .popover input").change(function () {
+    if ($(this).is(":checked")) {
+      amenity[$(this).attr("data-name")] = $(this).attr("data-id");
+    } else if ($(this).is(":not(:checked)")) {
+      delete amenity[$(this).attr("data-name")];
+    }
+    const names = Object.keys(amenity);
+    $(".amenities h4").text(names.sort().join(", "));
+  });
+});
